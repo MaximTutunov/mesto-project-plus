5ce33c4a6e1b65a21556c7803af0013716fef372
MaximTutunov <97736607+MaximTutunov@users.noreply.github.com>
2023-05-03 15:51:32 +0300

    9

--- src/controllers/cards.ts
+++ src/controllers/cards.ts
@@ -1,8 +1,9 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import mongoose from 'mongoose';
 import Card from '../models/card';
 import { ICustomRequest } from '../types';
 import STATUS_CODES from '../utils/constants';
+import user from 'models/user';
 
 export const getCards = async (_req: Request, res: Response) => {
   try {
@@ -33,32 +34,22 @@ export const createCard = async (req: ICustomRequest, res: Response) => {
   }
 };
 
-export const deleteCard = async (req: Request, res: Response) => {
+export const deleteCard = async (req: ICustomRequest, res: Response, next:NextFunction) => {
   const { cardId } = req.params;
+  const userId = req.user?._id;
   try {
-    const card = await Card.findByIdAndDelete(cardId);
+    const card = await Card.findById(cardId).populate({path:'owner', match:{_id:userId}}).orFail(new NotFoundError('Карточка с таким id не найдена'));
 
-    if (!card) {
-      const error = new Error('Карточка с данным id отсутствует');
-      error.name = 'CardNotFound';
-      throw error;
+    if (!card.owner) {
+
+      throw new ForbiddenError('Карточка с таким id принадлежит другому пользователю');
     }
+
+   await Card.findByIdAndDelete(cardId)
 
     return res.status(STATUS_CODES.OK).send({ message: 'Карточка удалена' });
   } catch (error) {
-    if (error instanceof Error && error.name === 'CardNotFound') {
-      return res
-        .status(STATUS_CODES.NOT_FOUND)
-        .send({ message: 'Карточка с данным id отсутствует' });
-    }
-    if (error instanceof Error && error.name === 'CastError') {
-      return res
-        .status(STATUS_CODES.BAD_REQUEST)
-        .send({ message: '_id карточки некорректный' });
-    }
-    return res
-    .status(STATUS_CODES.DEFAULT_ERROR)
-    .send({ message: 'Ошибка сервера' });
+    return next(error)
   }
 };
 
